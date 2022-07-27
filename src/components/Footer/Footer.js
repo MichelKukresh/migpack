bdd02b00dccf20637bcb4f716e003efc60dc4cc0
Михаил Кукреш <kukreshmihal@yandex.ru>
2022-07-27 09:53:24 +0300

    feature: add footer mnemonik

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,30 +1,61 @@
 import styles from "./Footer.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMapMarker } from "@fortawesome/free-solid-svg-icons";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 
 function Footer() {
   return (
-    <div className={styles.wrapper}>
+    
+
+<div className={styles.wrapper}>
       <ul>
-        информация
-        <li>О нас</li>
-        <li>Информация о доставке</li>
-        <li>Политика безопасности</li>
+        {" "}
+        <FontAwesomeIcon icon={faChevronRight} />
+        Информация
+        <li className={styles.chevron}>&rsaquo;О нас</li>
+        <li className={styles.chevron}>&rsaquo;Информация о доставке</li>
+        <li className={styles.chevron}>&rsaquo;Политика безопасности</li>
       </ul>
       <ul>
+        {" "}
+        <FontAwesomeIcon icon={faChevronRight} />
         Популярное
-        <li>Папки из картона</li>
-        <li>Крафт пакеты</li>
-        <li>Коробки самосборные</li>
-      </ul>
-      <ul>Дополнительно
-        <li>Дополнительно</li>
+        <li className={styles.chevron}>&rsaquo;Папки из картона</li>
+        <li className={styles.chevron}>&rsaquo;Крафт пакеты</li>
+        <li className={styles.chevron}>&rsaquo;Коробки самосборные</li>
       </ul>
       <ul>
-        Схема проезда
-        <li>Москва</li>
-        <li>телефон</li>
-        <li>почта</li>
+        {" "}
+        <FontAwesomeIcon icon={faChevronRight} />
+        Дополнительно{" "}
       </ul>
+      <ul>
+        {" "}
+        <FontAwesomeIcon icon={faChevronRight} />
+        Схема проезда
+        <li className={styles.chevron}>
+          <FontAwesomeIcon icon={faMapMarker} />
+          Москва
+        </li >
+        <li className={styles.chevron}>
+          <FontAwesomeIcon icon={faPhone} />
+          телефон
+        </li>
+        <li className={styles.chevron}>
+          <FontAwesomeIcon icon={faEnvelope} />
+          почта
+        </li>
+      </ul>
+      <div className={styles.footer__text}>Изготовление полиграфической продукции в Москве</div>
     </div>
+
+    
+
+
+    
+    
   );
 }
 
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -14,11 +14,27 @@ function Main() {
         <ProductCard></ProductCard>
         <ProductCard></ProductCard>
       </div>
-      
-      <div className={styles.heading}>
-      <div className={styles.hedingTitleAfter}><span className={styles.hedingTitle}>Услуги типографии</span></div>
-      <span className={styles.hedingSubTitle}>Наша типография занимается производством и продажей полиграфической продукции. Бумажные пакеты, полиэтиленовые пакеты, коробки, папки - основное направление деятельности.Типография  располагает собственной производственной базой по изготовлению продукции под ключ.</span>
 
+      <div className={styles.heading}>
+        <div className={styles.hedingTitleAfter}>
+          <span className={styles.hedingTitle}>Услуги типографии</span>
+        </div>
+        <span className={styles.hedingSubTitle}>
+          Наша типография занимается производством и продажей полиграфической
+          продукции. Бумажные пакеты, полиэтиленовые пакеты, коробки, папки -
+          основное направление деятельности.Типография располагает собственной
+          производственной базой по изготовлению продукции под ключ.
+        </span>
+      </div>
+      <div className={styles.heading}>
+        <div className={styles.hedingTitleAfter}>
+          <span className={styles.hedingTitle}>Вы недавно смотрели</span>
+        </div>        
+      </div>
+      <div className={styles.card__container}>
+        <ProductCard></ProductCard>
+        <ProductCard></ProductCard>
+        <ProductCard></ProductCard>        
       </div>
     </div>
   );
